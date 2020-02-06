0971f64d9d0bdd505ce372a801b36ea32cc1f93a
Your Name <chenyihongyi@163.com>
2020-02-06 08:06:23 +0800

    微信扫码登录，回调地址

--- src/main/java/com/video/demo/video_demo/controller/WechatController.java
+++ src/main/java/com/video/demo/video_demo/controller/WechatController.java
@@ -4,11 +4,13 @@ import com.video.demo.video_demo.config.WeChatConfig;
 import com.video.demo.video_demo.domain.JsonData;
 import com.video.demo.video_demo.domain.User;
 import com.video.demo.video_demo.service.UserService;
+import com.video.demo.video_demo.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -34,7 +36,7 @@ public class WechatController {
      */
     @GetMapping("login_url")
     @ResponseBody
-    public JsonData loginUrl(@RequestParam(value = "access_page",required = true)String accessPage) throws UnsupportedEncodingException {
+    public JsonData loginUrl(@RequestParam(value = "access_page",required = true)String accessPage) throws IOException {
 
         String redirectUrl = weChatConfig.getOpenRedirectUrl(); //获取开放平台重定向地址
 
@@ -45,13 +47,24 @@ public class WechatController {
         return JsonData.buildSuccess(qrcodeUrl);
     }
 
+    /**
+     * 微信扫码登录，回调地址
+     * @param code
+     * @param state
+     * @param response
+     * @throws IOException
+     */
     @GetMapping("/user/callback")
-    public void wechatUserCallback(@RequestParam(value = "code", required = true) String code,String state,HttpServletResponse response) {
+    public void wechatUserCallback(@RequestParam(value = "code", required = true) String code,String state,HttpServletResponse response) throws IOException {
 //        System.out.println("code="+code);
 //        System.out.println("state="+state);
         User user = userService.saveWeChatUser(code);
         if (user != null) {
             //生成jwt
+            String token = JwtUtils.geneJsonWebToken(user);
+
+            // state 当前用户的页面地址，需要拼接 http://  这样才不会站内跳转
+            response.sendRedirect(state+"?token="+token+"&head_img="+user.getHeadImg()+"&name="+URLEncoder.encode(user.getName(),"UTF-8"));
         }
     }
 }
